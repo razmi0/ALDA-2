2793eb65dec49c19cd8b13e693a0a541e0288404
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-04-30 01:24:15 +0200

    checkpoint : 30/04/2024: 1:24:14,96

---
+++ Makefile
@@ -0,0 +1,8 @@
+# WINDOWS VERSION (There's no /bin/date on Windows)
+DATE_WITH_TIME = $(shell echo %date%:%time%)
+
+checkpoint:
+	@git add -A
+	@git commit -m "checkpoint : $(DATE_WITH_TIME)"
+	@git push
+	@echo "checkpoint done!"
---
+++ eslint.config.js
@@ -0,0 +1,57 @@
+import js from "@eslint/js";
+import typescriptParser from "@typescript-eslint/parser";
+import astroEslintParser from "astro-eslint-parser";
+import eslintPluginAstro from "eslint-plugin-astro";
+import a11y from "eslint-plugin-jsx-a11y";
+
+export default [
+  js.configs.recommended,
+  ...eslintPluginAstro.configs["flat/recommended"],
+
+  {
+    files: ["**/*.{js,jsx,astro}"],
+    plugins: { "jsx-a11y": a11y },
+    rules: {
+      ...a11y.configs.recommended.rules,
+    },
+  },
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parser: astroEslintParser,
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+    },
+  },
+  {
+    files: ["**/*.{js,jsx,astro}"],
+    rules: {
+      "no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
+    },
+  },
+  {
+    // Define the configuration for `<script>` tag.
+    // Script in `<script>` is assigned a virtual file name with the `.js` extension.
+    files: ["**/*.{ts,tsx}", "**/*.astro/*.js"],
+    languageOptions: {
+      parser: typescriptParser,
+    },
+    rules: {
+      // Note: you must disable the base rule as it can report incorrect errors
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/no-non-null-assertion": "off",
+    },
+  },
+  {
+    ignores: ["dist", "node_modules", ".github", "types.generated.d.ts"],
+  },
+];
