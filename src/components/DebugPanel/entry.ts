e12d2b97d65d709a469570a95534c210e8db001d
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-05-02 18:36:48 +0200

    Add Target type to DebugPanelProps

--- src/components/DebugPanel/entry.ts
+++ src/components/DebugPanel/entry.ts
@@ -2,6 +2,11 @@ import type { CSSProperty } from "astro/types";
 
 type Unit = "px" | "%" | "deg" | "em" | "rem";
 
+export type Target = {
+  tag: string;
+  element: Element;
+} | null;
+
 export interface DebugPanelProps {
   range?: {
     targetTag: string;
