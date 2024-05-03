e03c8a58c474db7fcb10e0a5d3f0d83466dd246d
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-05-03 16:39:46 +0200

    Add location.json and types.ts files for LocationExplorer component

---
+++ src/components/LocationExplorer/types.ts
@@ -0,0 +1,9 @@
+export interface Location {
+  title: string; // location name
+  subtitle: string; // location type
+  coordinates: [number, number];
+  page: number;
+  labelPage: `${this["page"]}` & string;
+  label?: string | undefined; // location label on map
+  selected?: boolean | undefined;
+}
