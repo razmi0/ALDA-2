5c637716352fbf05a1c781f82f53cb0bc9e33c7c
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-05-07 16:01:54 +0200

    checkpoint : 07/05/2024:16:01:54,03

--- src/components/Location/LocationExplorer/types.ts
+++ src/components/Location/LocationExplorer/types.ts
@@ -2,8 +2,8 @@ export interface Location {
   title: string; // location name
   subtitle: string; // location type
   coordinates: [number, number];
-  page: number;
-  labelPage: `${this["page"]}` & string;
+  page?: number;
+  labelPage?: `${this["page"]}` & string;
   label?: string | undefined; // location label on map
   selected?: boolean | undefined;
 }
