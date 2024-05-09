4992d6d56d91951b5b09af1e033faf29dd058a9b
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-05-09 17:18:23 +0200

    Add prop to products, more related types, refactor accordingly

--- src/components/Products/types.ts
+++ src/components/Products/types.ts
@@ -1,8 +1,28 @@
 export interface ProductType {
   id: number;
+  name: string;
   title: string;
-  description: string;
+  description: DescriptionType;
   price: string;
-  unit: string;
-  rating: string;
+  unit: UnitType;
+  rating: RatingType;
+  stats: StatsType;
+
+  tags: string[];
+  footerText: string;
 }
+
+type StatsType = [
+  { label: "courage"; value: string },
+  { label: "agilite"; value: string },
+  { label: "experience"; value: string }
+];
+
+type RatingType = "1" | "2" | "3" | "4" | "5";
+
+type DescriptionType = {
+  short: string;
+  long: string;
+};
+
+type UnitType = "group" | "person" | "enfant" | "nuit" | "journée";
