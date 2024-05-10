b52f353f1a8f8a6e48bd2f42490100d5ff591822
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-05-10 13:17:32 +0200

    checkpoint : 10/05/2024:13:17:32,62

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -13,31 +13,32 @@ export function DatePickerWithPresets() {
   const [date, setDate] = React.useState<Date>();
 
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant={"outline"}
-          className={cn("w-[240px] justify-start text-left font-normal", !date && "text-muted-foreground")}>
-          <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? format(date, "PPP") : <span>Pick a date</span>}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent align="start" className="flex w-auto flex-col space-y-2 p-2">
-        <Select onValueChange={(value) => setDate(addDays(new Date(), parseInt(value)))}>
-          <SelectTrigger>
-            <SelectValue placeholder="Select" />
-          </SelectTrigger>
-          <SelectContent position="popper">
-            <SelectItem value="0">Today</SelectItem>
-            <SelectItem value="1">Tomorrow</SelectItem>
-            <SelectItem value="3">In 3 days</SelectItem>
-            <SelectItem value="7">In a week</SelectItem>
-          </SelectContent>
-        </Select>
-        <div className="rounded-md border">
-          <Calendar mode="single" selected={date} onSelect={setDate} />
-        </div>
-      </PopoverContent>
-    </Popover>
+    <div className="vertical">
+      <span className="font-normal text-sm mb-2">Choisissez une date :</span>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button variant={"outline"} className={cn("w-[240px] justify-start text-left font-normal")}>
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {date ? format(date, "PPP") : <></>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent align="start" className="flex w-auto flex-col space-y-2 p-2">
+          <Select onValueChange={(value) => setDate(addDays(new Date(), parseInt(value)))}>
+            <SelectTrigger>
+              <SelectValue placeholder="Select" />
+            </SelectTrigger>
+            <SelectContent position="popper">
+              <SelectItem value="0">Today</SelectItem>
+              <SelectItem value="1">Tomorrow</SelectItem>
+              <SelectItem value="3">In 3 days</SelectItem>
+              <SelectItem value="7">In a week</SelectItem>
+            </SelectContent>
+          </Select>
+          <div className="rounded-md border">
+            <Calendar mode="single" selected={date} onSelect={setDate} />
+          </div>
+        </PopoverContent>
+      </Popover>
+    </div>
   );
 }
