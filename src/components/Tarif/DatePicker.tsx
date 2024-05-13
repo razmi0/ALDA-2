86bc403081d84f3698b26564948905938094c54d
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-05-13 16:56:21 +0200

    checkpoint : 13/05/2024:16:56:21,52

--- src/components/Tarif/DatePicker.tsx
+++ src/components/Tarif/DatePicker.tsx
@@ -9,24 +9,19 @@ import { cn } from "@/lib/utils";
 import { Calendar as CalendarIcon } from "lucide-react";
 export function DatePickerWithPresets() {
   const [date, setDate] = React.useState<Date>();
-  const dialogRef = React.useRef<HTMLDialogElement>(null);
 
   return (
     <div className="vertical center">
       <label className="mb-1 text-left text-sm w-full">Réservez une date :</label>
-      <Button
-        onClick={() => {
-          if (dialogRef.current?.open) dialogRef.current?.close();
-          dialogRef.current?.showModal();
-        }}
-        variant={"outline"}
-        className={cn("w-full justify-start text-left font-normal max-w-72")}>
+      <Button type="button" variant={"outline"} className={cn("w-full justify-start text-left font-normal max-w-72")}>
         <CalendarIcon className="mr-2 h-4 w-4" />
         {date ? format(date, "PPP") : <></>}
       </Button>
+      <input type="hidden" name="date" value={date?.toDateString()} />
 
       <div className="rounded-md border max-w-72">
         <Calendar mode="single" selected={date} onSelect={setDate} className="bg-white rounded-md max-w-72" />
+        in
       </div>
     </div>
   );
