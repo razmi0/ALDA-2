6d8306f5502f5610db10df04414b28febbf01838
razmiO <thomas.cuesta31@gmail.com>
2025-01-24 13:36:04 +0100

    style : switch to vertical menu

--- src/components/Nav/MobileMenu.tsx
+++ src/components/Nav/MobileMenu.tsx
@@ -1,36 +1,36 @@
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { cn } from "@/lib/utils";
 import { MenuIcon } from "lucide-react";
 import type { WebsiteLink } from "./types";
 
 type MobileMenuProps = {
-  websiteLinks: WebsiteLink[];
+    websiteLinks: WebsiteLink[];
 };
 
 const MobileMenu = ({ websiteLinks }: MobileMenuProps) => {
-  return (
-    <section className="lg:hidden flex">
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <MenuIcon className="w-9 h-9 text-white hover:text-white/80" />
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="inline-flex gap-5">
-          {websiteLinks.map(({ href, isActive, name }) => (
-            <DropdownMenuItem key={name} className="font-bold text-[1rem]">
-              <a href={href} className={cn(isActive ? "text-pur-500" : "")}>
-                {name}
-              </a>
-            </DropdownMenuItem>
-          ))}
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </section>
-  );
+    return (
+        <section className="lg:hidden flex">
+            <DropdownMenu>
+                <DropdownMenuTrigger>
+                    <MenuIcon className="w-9 h-9 text-white hover:text-white/80" />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="mr-3">
+                    {websiteLinks.map(({ href, isActive, name }) => (
+                        <DropdownMenuItem key={name} className="font-bold text-[1rem]">
+                            <a href={href} className={cn(isActive ? "text-pur-500" : "")}>
+                                {name}
+                            </a>
+                        </DropdownMenuItem>
+                    ))}
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </section>
+    );
 };
 
 export default MobileMenu;
