71a473f5ed5fcd87650f6ab74c166bf19ffd0a39
razmiO <thomas.cuesta31@gmail.com>
2025-01-25 19:26:11 +0100

    chore: move section around

--- src/components/Home/Product/types.ts
+++ src/components/Home/Product/types.ts
@@ -1,53 +1,40 @@
 export type ProductImage = {
-  label: Product["title"];
-  src: ImageMetadata;
-  alt: string;
-  size: number[];
+    label: Product["title"];
+    src: ImageMetadata;
+    alt: string;
+    size: number[];
 };
-
 export interface ProductData {
-  poles: Poles;
-  products: Product[];
+    poles: Poles;
+    products: Product[];
 }
 
 export interface Poles {
-  available: number[];
-  simple: number[];
-  complex: number[];
-  all: All[];
+    available: number[];
+    all: All[];
 }
 
 export interface All {
-  id: number;
-  label: string;
+    id: number;
+    label: string;
 }
 
 export interface Product {
-  id: number;
-  title: string;
-  pole: string;
-  tags?: string[];
-  description?: string;
-  price?: string;
-  program?: string;
-  audience?: string;
-  locations?: string;
+    id: number;
+    title: string;
+    pole: string;
+    tags?: string[];
+    description?: string;
+    price?: string;
+    program?: string;
+    audience?: string;
+    locations?: string;
+    subproducts?: Subproduct[];
 }
 
-export interface ComplexProduct extends Product {
-  data: Datum[];
-}
-
-export interface Datum {
-  id: number;
-  title: string;
-  subtitle: string;
-  objectifs: string[];
-  ateliers: Atelier[];
-}
-
-export interface Atelier {
-  title: string;
-  duration: string;
-  activities?: string[];
+export interface Subproduct {
+    id: number;
+    title: string;
+    theme: string;
+    description: string;
 }
