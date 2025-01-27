170bdc6e11c1fb3d0f747c20ecba6e1c08505c8d
razmiO <thomas.cuesta31@gmail.com>
2025-01-27 19:49:40 +0100

    fix:redirections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
+
 export default defineConfig({
-    redirects: {
-        "/": "/maintenance",
-        "/activités": "/maintenance",
-        "/contact": "/maintenance",
-        "/faq": "/maintenance",
-        "/nous": "/maintenance",
-    },
+    redirects: {},
     integrations: [
         react(),
         tailwind({
