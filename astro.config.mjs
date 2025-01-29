ed6d0955b94ae017fa3142bcfe0244ac37293b56
razmiO <thomas.cuesta31@gmail.com>
2025-01-29 23:06:53 +0100

    status : website offline

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,23 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
 
+/**
+ * redirections to maintenance page if true;
+ */
+const OFFLINE_PROD = import.meta.env.PROD && true;
+
 // https://astro.build/config
 
+const maintenancyRedirections = {
+    "/": "/maintenance",
+    "/contact": "/maintenance",
+    "/activités": "/maintenance",
+    "/faq": "/maintenance",
+    "/nous": "/maintenance",
+};
+
 export default defineConfig({
-    redirects: {},
+    redirects: OFFLINE_PROD ? maintenancyRedirections : {},
     integrations: [
         react(),
         tailwind({
