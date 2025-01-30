e2716bc7ee8e8d6731d45fabdf0e7b54e4a18f1f
razmiO <thomas.cuesta31@gmail.com>
2025-01-30 21:53:43 +0100

    feat:sitemap configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
 
+import sitemap from "@astrojs/sitemap";
+
 /**
  * redirections to maintenance page if true;
  */
@@ -13,17 +15,32 @@ const OFFLINE_PROD = import.meta.env.PROD && true;
 const maintenancyRedirections = {
     "/": "/maintenance",
     "/contact": "/maintenance",
-    "/activités": "/maintenance",
+    "/activites": "/maintenance",
     "/faq": "/maintenance",
     "/nous": "/maintenance",
 };
 
 export default defineConfig({
-    redirects: OFFLINE_PROD ? maintenancyRedirections : {},
+    site: "https://alabordarbre.fr",
+    // redirects: OFFLINE_PROD ? maintenancyRedirections : {},
     integrations: [
         react(),
         tailwind({
             applyBaseStyles: false,
+        }),
+        sitemap({
+            /**
+             * Check if the page is not crawlable
+             * @param {`https://alabordarbre.fr/${string}/`} page
+             * @returns {boolean}
+             */
+            filter: (page) => {
+                const forbiddenPaths = ["/honey", "/maintenance"];
+                const url = new URL(page);
+                const crawlable = !forbiddenPaths.some((path) => url.pathname.startsWith(path));
+                console.log("[Generating sitemap] ", page, crawlable ? "crawlable" : "not crawlable");
+                return crawlable;
+            },
         }),
     ],
 
